804b0330e15008660b72ba662e9a9edd770a97a7
Nisad Islam <nisadnn2001@gmail.com>
2026-03-01 14:41:04 +0600

    improve banner card section

--- src/Components/Section/Banner/BannerCard/BannerCard.jsx
+++ src/Components/Section/Banner/BannerCard/BannerCard.jsx
@@ -3,75 +3,65 @@ import { FaArrowRight } from "react-icons/fa";
 
 const BannerCard = () => {
   return (
-    <div className='max-w-[1170px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[30px]'>
+    <div className='max-w-[1170px] mx-auto px-4 lg:px-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[30px]'>
       {/* Card 1 */}
-      <div className='py-[30px] pr-[98px] pl-[30px] bg-[#FFFFFF] shadow-[20px_40px_80px_0px_rgba(0,0,0,0.25)] relative rounded-[4px]'>
+      <div className='py-[30px] pr-[30px] lg:pr-[98px] pl-[30px] bg-white shadow-[20px_40px_80px_rgba(0,0,0,0.25)] relative rounded-[4px]'>
         <h1 className='font-semibold text-2xl leading-[120%] text-[#4D4C50] mb-2'>
           Business Consulting
         </h1>
-        <p className='text-[#7A798B] font-normal text-[14px] leading-[140%]'>
+
+        <p className='text-[#7A798B] text-[14px] leading-[140%]'>
           We provide the best services to <br />
           ensure the high-level success of your business
         </p>
 
-        <span
-          className='absolute bottom-[10px] right-[20px] 
-          bg-[#6140FF] rounded-full text-white 
-          w-[38px] h-[38px] 
-          flex items-center justify-center'
-        >
+        {/* Arrow */}
+        <span className='absolute bottom-[10px] right-[20px] bg-[#6140FF] rounded-full text-white w-[38px] h-[38px] flex items-center justify-center'>
           <FaArrowRight />
         </span>
 
-        <div className='absolute top-0 left-[290px]'>
+        {/* Image (Hidden on small device) */}
+        <div className='hidden lg:block absolute top-0 left-[290px]'>
           <img src='/assets/images/bannercardImg.png' alt='' />
         </div>
       </div>
 
       {/* Card 2 */}
-      <div className='py-[30px] pr-[98px] pl-[30px] bg-[#FFFFFF] shadow-[20px_40px_80px_0px_rgba(0,0,0,0.25)] relative rounded-[4px]'>
+      <div className='py-[30px] pr-[30px] lg:pr-[98px] pl-[30px] bg-white shadow-[20px_40px_80px_rgba(0,0,0,0.25)] relative rounded-[4px]'>
         <h1 className='font-semibold text-2xl leading-[120%] text-[#4D4C50] mb-2'>
           Business Consulting
         </h1>
-        <p className='text-[#7A798B] font-normal text-[14px] leading-[140%]'>
+
+        <p className='text-[#7A798B] text-[14px] leading-[140%]'>
           We provide the best services to <br />
           ensure the high-level success of your business
         </p>
 
-        <span
-          className='absolute bottom-[10px] right-[20px] 
-          bg-[#6140FF] rounded-full text-white 
-          w-[38px] h-[38px] 
-          flex items-center justify-center'
-        >
+        <span className='absolute bottom-[10px] right-[20px] bg-[#6140FF] rounded-full text-white w-[38px] h-[38px] flex items-center justify-center'>
           <FaArrowRight />
         </span>
 
-        <div className='absolute top-0 left-[290px]'>
+        <div className='hidden lg:block absolute top-0 left-[290px]'>
           <img src='/assets/images/bannercardImg.png' alt='' />
         </div>
       </div>
 
       {/* Card 3 */}
-      <div className='py-[30px] pr-[98px] pl-[30px] bg-[#FFFFFF] shadow-[20px_40px_80px_0px_rgba(0,0,0,0.25)] relative rounded-[4px]'>
+      <div className='py-[30px] pr-[30px] lg:pr-[98px] pl-[30px] bg-white shadow-[20px_40px_80px_rgba(0,0,0,0.25)] relative rounded-[4px]'>
         <h1 className='font-semibold text-2xl leading-[120%] text-[#4D4C50] mb-2'>
           Business Consulting
         </h1>
-        <p className='text-[#7A798B] font-normal text-[14px] leading-[140%]'>
+
+        <p className='text-[#7A798B] text-[14px] leading-[140%]'>
           We provide the best services to <br />
           ensure the high-level success of your business
         </p>
 
-        <span
-          className='absolute bottom-[10px] right-[20px] 
-          bg-[#6140FF] rounded-full text-white 
-          w-[38px] h-[38px] 
-          flex items-center justify-center'
-        >
+        <span className='absolute bottom-[10px] right-[20px] bg-[#6140FF] rounded-full text-white w-[38px] h-[38px] flex items-center justify-center'>
           <FaArrowRight />
         </span>
 
-        <div className='absolute top-0 left-[290px]'>
+        <div className='hidden lg:block absolute top-0 left-[290px]'>
           <img src='/assets/images/bannercardImg.png' alt='' />
         </div>
       </div>
